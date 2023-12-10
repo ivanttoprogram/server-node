0f50e395c3a12eb56da533c471dd6717e2f7276f
Cheryl Lopez <clopez51@stu.southtexascollege.edu>
2023-12-10 14:45:08 -0600

    Added docker file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+// eslint-disable-next-line import/extensions
+import './helpers/dotenv.js'
+
 import express from 'express'
+import morgan from 'morgan'
+
+const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
-const port = 3000
+
+app.use(morgan(process.env.MORGAN_LOG))
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Hello There' })
+  const title = process.env.TITLE || 'Server'
+  res.send({ msg: title })
 })
 
 app.listen(port)
